66b3f108c8dfc35d072d4840fb8089dc072705f5
Anna Gubina <anya.gubina101@gmail.com>
2025-05-30 12:51:44 -0400

    Первая сдача итоговой

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -8,8 +8,9 @@ class User {
    * Устанавливает текущего пользователя в
    * локальном хранилище.
    * */
+  static URL = '/user'
   static setCurrent(user) {
-
+    localStorage.setItem = ('user', 'user')
   }
 
   /**
@@ -17,7 +18,7 @@ class User {
    * пользователе из локального хранилища.
    * */
   static unsetCurrent() {
-
+    localStorage.removeItem('user')
   }
 
   /**
@@ -25,7 +26,7 @@ class User {
    * из локального хранилища
    * */
   static current() {
-
+    localStorage.getItem('user') || undefined;
   }
 
   /**
@@ -33,7 +34,20 @@ class User {
    * авторизованном пользователе.
    * */
   static fetch(callback) {
-
+      createRequest({
+      url : this.URL + '/current',
+      method : 'GET',
+      data,
+      callback(err, response) {
+        if(response && response.user) {
+          this.setCurrent(response.user)
+        } else {
+          this.unsetCurrent()
+        }
+        
+        callback(err, response);
+      }
+    })
   }
 
   /**
@@ -64,7 +78,18 @@ class User {
    * User.setCurrent.
    * */
   static register(data, callback) {
-
+    createRequest({
+      url: this.URL + '/register',
+      method: 'POST',
+      responseType: 'json',
+      data,
+      callback: (err, response) => {
+        if (response && response.user) {
+          this.setCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
   }
 
   /**
@@ -72,6 +97,17 @@ class User {
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout(callback) {
-
+    createRequest({
+      url: this.URL + '/logout',
+      method: 'POST',
+      responseType: 'json',
+      data,
+      callback: (err, response) => {
+        if (response && response.user) {
+          this.unsetCurrentsetCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,27 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest;
+    const formData = new FormData;
+    xhr.responseType = 'json';
+    const urlOptions = options.url;
+    const arrayFromObj = Object.entries(options.data);
 
+    if(options.method === 'GET') {
+        urlOptions += '?'
+        arrayFromObj.forEach((key, value) => {
+            urlOptions + `${key}=${value}`
+        })
+    } else {
+        arrayFromObj.forEach((key, value) => {
+            formData.append(key, value)
+        })
+    }
+
+    xhr.onload(() => {
+        options.callback(null, xhr.response)
+    })
+
+    xhr.open(options.method, urlOptions);
+    xhr.send(formData)
 };
